68c0356e9076971a8e9906af680e03bbfafbf9b9
Arya Rajput <rajputarya10@gmail.com>
2025-03-11 00:09:24 +0530

    db variable name correction

--- firebase.ts
+++ firebase.ts
@@ -17,6 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-const dp = getFirestore(app)
+const db = getFirestore(app)
 
-export { dp }
+export { db }
