cc1a336c5c49d77d30de920f8292575643616096
Arya Rajput <rajputarya10@gmail.com>
2025-03-14 22:11:00 +0530

    removed build issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  plugins: {
+    '@tailwindcss/postcss': {
+      optimize: {
+        minify: false
+      }
+    }
+  },
 };
 
 export default nextConfig;
