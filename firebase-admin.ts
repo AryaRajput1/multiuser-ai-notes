e334184fe4950125f5d66da0a4080a347469869f
Arya Rajput <rajputarya10@gmail.com>
2025-03-14 22:37:25 +0530

    added service key

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -3,6 +3,9 @@ import { getFirestore } from "firebase-admin/firestore"
 
 var serviceKey = require("./service_key.json");
 
+serviceKey['private_key'] = process.env.FIREBASE_ADMIN_PRIVATE_KEY
+serviceKey['private_key_id'] = process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID
+
 let app:App;
 
 if( getApps().length === 0 ) {
